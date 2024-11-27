f9707d4dff8a0d61332552af5df1d2acde3bbc1b
Ahmillion <ahmilb@nycstudents.net>
2024-11-27 08:59:00 -0500

    COMPLETED PART 12

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -6,12 +6,19 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {   
     public InputAction MoveAction;
+   Rigidbody2D rigidbody2d;
+   Vector2 move;
+   public float speed = 3.0f;
    
-   
+   public int maxHealth = 5;
+   int currentHealth = 1;
+
     // Start is called before the first frame update
     void Start()
     {
      MoveAction.Enable();
+     rigidbody2d = GetComponent<Rigidbody2D>();
+     //currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -19,8 +26,18 @@ public class PlayerController : MonoBehaviour
     {
         
        Vector2 move = MoveAction.ReadValue<Vector2>();
-     Debug.Log(move);
-     Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
-     transform.position = position;     
+      Debug.Log(move);    
     }
+
+    void FixedUpdate()
+
+  {
+    Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
+    rigidbody2d.MovePosition(position);
+  }
+ public void ChangeHealth (int amount)
+  {
+    currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+    Debug.Log(currentHealth + "/" + maxHealth);
+  }
 }
